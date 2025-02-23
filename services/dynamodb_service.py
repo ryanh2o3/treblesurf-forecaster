b0d2af599816354d03f4d4bd98ec69febb668360
Ryan Patton <ryancpatton0@gmail.com>
2025-02-23 14:04:16 +0000

    lambda forecaster

---
+++ services/dynamodb_service.py
@@ -0,0 +1,57 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'SurfSpotForecastData'
+table = dynamodb.Table(table_name)
+
+def save_forecast_data(forecast_data, forecast_date, country, region, spot):
+    sort_key = f"{country}_{region}_{spot}"
+    item = {
+        'forecastDate': forecast_date,
+        'country_region_spot': sort_key,
+        'data': forecast_data
+    }
+    
+    try:
+        table.put_item(Item=item)
+        return True
+    except ClientError as e:
+        print(f"Error saving data to DynamoDB: {e.response['Error']['Message']}")
+        return False
+    
+
+def get_location_data(table_name='LocationData'):
+    """Fetch all surf spot locations from DynamoDB"""
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    
+    response = table.scan()
+    locations = response['Items']
+    
+    # Handle pagination if there are more items
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        locations.extend(response['Items'])
+    
+    return locations
+
+def parse_location_data(location):
+    """Parse location data into required format"""
+    country_region_spot = location['country_region_spot']
+    parts = country_region_spot.split('/')
+    
+    # Parse IdealSwellDirection string into tuple
+    ideal_swell = location['IdealSwellDirection'].strip('"').split(',')
+    ideal_swell = (float(ideal_swell[0]), float(ideal_swell[1]))
+    
+    return {
+        'country': parts[0],
+        'region': parts[1],
+        'spot': parts[2],
+        'beach_direction': float(location['BeachDirection']),
+        'latitude': float(location['Latitude']),
+        'longitude': float(location['Longitude']),
+        'ideal_swell_direction': ideal_swell
+    }
